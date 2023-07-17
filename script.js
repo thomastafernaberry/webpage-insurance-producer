5155209f8fdc2b1ba459d378912491cee4f8c193
Thomas Tafernaberry <thomastafernaberry@gmail.com>
2023-07-17 03:28:14 -0300

    add new page. improve CX

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const $BODY = document.querySelector('body');
 const $BUTTON = document.querySelector('#main');
+const $SECTION = document.querySelectorAll('section');
+const $BURGER_MENU_BUTTON = document.querySelector('.burger-menu');
+const $NAV_LINKS = document.querySelector('.nav-links');
 
 function scrollToElement(element) {
+    const scrollHeight = element.offsetTop - 200;
     scroll({
-        top:900,
+        top:scrollHeight,
         behavior: 'smooth',
     });
 }
-function getScrollPosition() {
-    return window.scrollY;
+function scrollToSection() {
+    for (let i = 0; i > $SECTION.length; i++ ){
+        console.log(`executed. number ${i}`);
+        scrollToElement($SECTION[i]);
+    }
 }
 $BUTTON.onclick = function() {
-    scrollToElement();
-    console.log(getScrollPosition());
+    scrollToElement($SECTION[1]);
 };
+
+$BURGER_MENU_BUTTON.onclick = function() {
+    displayLinks = $NAV_LINKS.style.display;
+    isMenuOpen = displayLinks != "none";
+    isMenuOpen  ?  displayLinks = "none" : displayLinks = "block";
+}
+
+
 
